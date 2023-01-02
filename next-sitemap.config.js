5a8aec8da106dc44063552b995f886e248742c28
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-01-02 14:40:41 +0500

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://paintigo.shop',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
