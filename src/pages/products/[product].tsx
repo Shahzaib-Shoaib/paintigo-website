eb76a24a9a3c901fe66a09d3ce61eda9c1adc030
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-01-04 20:08:00 +0500

    .

--- src/pages/products/[product].tsx
+++ src/pages/products/[product].tsx
@@ -5,50 +5,51 @@ import { GetServerSideProps } from "next";
 import { getAllProducts, getProduct } from "@lib/shopify";
 import ProductPageContent from "@components/product/product-page-content";
 
-
-export default function ProductPage({ product }:any) {
-
-
-	return (
-		<div className='min-h-screen py-12 sm:pt-20'>
-		<ProductPageContent product={product} />
-	  </div>
-	);
+export default function ProductPage({ product }: any) {
+	console.log(product);
+	
+  return (
+    <>
+      <div className="min-h-screen py-12 sm:pt-20">
+        <ProductPageContent product={product} />
+      </div>
+    </>
+  );
 }
 
 ProductPage.Layout = Layout;
 
 export async function idk() {
-	const products = await getAllProducts();
-	const paths = products.map((item:any) => {
-		const product = String(item.node.handle);
-	
-		return {
-		  params: { product },
-		};
-	  });
-	  return {
-		paths,
-		fallback: false,
-	  };
+  const products = await getAllProducts();
+  const paths = products.map((item: any) => {
+    const product = String(item.node.handle);
 
+    return {
+      params: { product },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  params,
+}: any) => {
+  const product = await getProduct(params.product, locale);
 
-export const getServerSideProps: GetServerSideProps = async ({ locale,params }:any) => {
-	const product = await getProduct(params.product,locale);
-
-	return {
-		props: {
-			...(await serverSideTranslations(locale!, [
-				"common",
-				"forms",
-				"menu",
-				"footer",
-				"aboutus",
-
-			])),
-			product
-		},
-	};
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, [
+        "common",
+        "forms",
+        "menu",
+        "footer",
+        "aboutus",
+      ])),
+      product,
+    },
+  };
 };
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -7,17 +7,46 @@ import { GetStaticProps } from "next";
 import ProductList from "@components/product/product-list";
 import { getProductsInCollection } from "@lib/shopify";
 import SocialMedia from "@components/common/socialmedia";
-
-
+import { NextSeo } from "next-seo";
 
 export default function Products({ products }: any) {
   const { t } = useTranslation("common");
-  
+
   return (
     <>
-    
+      <NextSeo
+        additionalMetaTags={[
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+          },
+        ]}
+        title="Products"
+        description="Paintigo Products are dedicated to giving you the very best of experience, with a focus on quality, pigment, color vibrancy, and long-lasting painting results."
+        canonical=""
+        openGraph={{
+          url: "",
+          title: "Products",
+          description:
+            "Paintigo Products are dedicated to giving you the very best of experience, with a focus on quality, pigment, color vibrancy, and long-lasting painting results.",
+          images: [
+            {
+              url: "",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+            },
+            {
+              url: "",
+              width: 900,
+              height: 800,
+              alt: "Og Image Alt Second",
+            },
+          ],
+        }}
+      />
+      
       <ProductList products={products} />
-
       <Container>
         <SocialMedia />
       </Container>
@@ -38,7 +67,6 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
         "menu",
         "footer",
         "aboutus",
-
       ])),
       products,
     },
