31d257bc7ecc68895d124d34ca215395556989d6
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-02-03 17:23:44 +0500

    .

--- src/components/product/product-card.tsx
+++ src/components/product/product-card.tsx
@@ -104,9 +104,9 @@ const ProductCard = ({ product }: any) => {
               <span className="text-2xl font-bold text-gray-900">
                 {formatter.format(price)}{" "}
               </span>
-              <span className="text-white bg-[#262626] hover:bg-gray-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              <button className="text-white bg-[#262626] hover:bg-gray-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                 View Details{" "}
-              </span>
+              </button>
             </div>
           </div>
         </div>
