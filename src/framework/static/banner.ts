b74c77c27952a69c00611b617d2c28f754c8cc6b
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-02-06 17:18:54 +0500

    .

--- src/framework/static/banner.ts
+++ src/framework/static/banner.ts
@@ -6,34 +6,34 @@ export const HomePage = [
       slug: '',
       image: {
         mobile: {
-          url: '/assets/images/banner/mobile-1.webp',
+          url: '/assets/images/banner/desktop-3.png',
           width: 375,
           height: 220,
         },
         desktop: {
-          url: '/assets/images/banner/desktop.webp',
+          url: '/assets/images/banner/desktop-3.png',
           width: 1920,
           height: 900,
         },
       },
     },
-    {
-      id: 2,
-      title: 'Banner 2',
-      slug: '',
-      image: {
-        mobile: {
-          url: '/assets/images/banner/mobile-2.webp',
-          width: 375,
-          height: 220,
-        },
-        desktop: {
-          url: '/assets/images/banner/desktop-2.webp',
-          width: 1920,
-          height: 900,
-        },
-      },
-    },
+    // {
+    //   id: 2,
+    //   title: 'Banner 2',
+    //   slug: '',
+    //   image: {
+    //     mobile: {
+    //       url: '/assets/images/banner/desktop-4.jpg',
+    //       width: 375,
+    //       height: 220,
+    //     },
+    //     desktop: {
+    //       url: '/assets/images/banner/desktop-4.jpg',
+    //       width: 1920,
+    //       height: 900,
+    //     },
+    //   },
+    // },
     // {
     //   id: 3,
     //   title: 'Banner 3',
