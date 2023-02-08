b1aaaaab8a28ea762c355cfaae76287516c3a9dc
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-02-08 12:38:46 +0500

    .

--- src/framework/static/banner.ts
+++ src/framework/static/banner.ts
@@ -17,23 +17,23 @@ export const HomePage = [
         },
       },
     },
-    // {
-    //   id: 2,
-    //   title: 'Banner 2',
-    //   slug: '',
-    //   image: {
-    //     mobile: {
-    //       url: '/assets/images/banner/desktop-4.jpg',
-    //       width: 375,
-    //       height: 220,
-    //     },
-    //     desktop: {
-    //       url: '/assets/images/banner/desktop-4.jpg',
-    //       width: 1920,
-    //       height: 900,
-    //     },
-    //   },
-    // },
+    {
+      id: 2,
+      title: 'Banner 2',
+      slug: '',
+      image: {
+        mobile: {
+          url: '/assets/images/banner/desktop-4.jpg',
+          width: 375,
+          height: 220,
+        },
+        desktop: {
+          url: '/assets/images/banner/desktop-4.jpg',
+          width: 1920,
+          height: 900,
+        },
+      },
+    },
     // {
     //   id: 3,
     //   title: 'Banner 3',
